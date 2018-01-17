17e77d18c2ef5459f5b5a33169eac5cf3d1829a1
Justin Bryeans <justinbryeans@Justins-MacBook-Pro.local>
2018-01-17 13:29:14 -0800

    Update resetButton to display appropriate textContent, update styling

--- colorGame.js
+++ colorGame.js
@@ -49,6 +49,7 @@ resetButton.addEventListener("click", function() {
 	pickedColor = pickColor();
 	//Change colorDisplay to match picked Color
 	colorDisplay.textContent = pickedColor;
+	this.textContent = "New Colors";
 	messageDisplay.textContent = "";
 	//Change colors of squares
 	for(i = 0; i < squares.length; i++) {
