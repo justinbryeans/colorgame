09cf233662ecc042d8f9f04796da657b6934ff9c
Justin Bryeans <justinbryeans@Justins-MBP.domain>
2018-01-16 18:34:16 -0800

    Add colorDisplay span so that H1 displays the current color information

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,18 @@
 var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
+	"RGB(255, 0, 0)",
+	"RGB(255, 255, 0)",
+	"RGB(0, 255, 0)",
+	"RGB(0, 255, 255)",
+	"RGB(0, 0, 255)",
+	"RGB(255, 0, 255)"
 ]
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
+var colorDisplay = document.querySelector("#colorDisplay");
 
 for(i = 0; i < squares.length; i++) {
 	squares[i].style.backgroundColor = colors[i];
 }
+
+colorDisplay.textContent = pickedColor;
